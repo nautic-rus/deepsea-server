17e25d335b84613a4c72eb51aa83cc6551df091c
bogdan <Whatab0utus>
2022-11-23 09:35:34 +0300

    some changes

--- src/deepsea/issues/IssueManagerHelper.scala
+++ src/deepsea/issues/IssueManagerHelper.scala
@@ -821,7 +821,7 @@ trait IssueManagerHelper extends MongoCodecs {
             val cloudFiles = DBManager.GetNextCloudConnection() match {
               case Some(cloudConnection) =>
                 val stmt = cloudConnection.createStatement()
-                val query = s"select * from oc_activity where file like '%$filter%' and subject = 'created_self' and object_id not in (select fileid from oc_filecache where path like '%/trash/%' or path like '%/arh/%')"
+                val query = s"select * from oc_activity where file like '%$filter%' and subject = 'created_self' and object_id not in (select fileid from oc_filecache where path like '%/trash/%' or path like '%/arh/%') and object_id not in (select object_id from oc_activity where subject = 'deleted_self')"
                 val resultSet = RsIterator(stmt.executeQuery(query))
                 val res = resultSet.map(rs => {
                   CloudFile(
