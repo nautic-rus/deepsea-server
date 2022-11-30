3279a610131c8171754f91d8afcf379217ade418
bogdan <Whatab0utus>
2022-11-30 13:44:29 +0300

    some changes

--- rsc/queries/cloud-files.sql
+++ rsc/queries/cloud-files.sql
@@ -6,4 +6,4 @@ select
     (select oa.user from oc_activity oa where object_id = fc.fileid and subject = 'created_self' limit 1) as user
 from
     oc_filecache fc
-where path  like '%&filter%' and path not like '%/trash/%'
+where path  like '%&filter%' and path not like '%/trash/%' and path not like '%/arh/%'
