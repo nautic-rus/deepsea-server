054d1b1a75307b155d16f4b6313a0126abad7e6f
bogdan <Whatab0utus>
2023-01-30 14:16:27 +0300

    some changes

--- src/deepsea/database/DBManager.scala
+++ src/deepsea/database/DBManager.scala
@@ -40,7 +40,7 @@ object DBManager extends MongoCodecs {
   configPG.setJdbcUrl("jdbc:postgresql://192.168.1.26/deepsea")
   configPG.setUsername("deepsea")
   configPG.setPassword("Ship1234")
-  configPG.setMaximumPoolSize(5)
+  configPG.setMaximumPoolSize(50)
   val dsPG = new HikariDataSource(configPG)
 
 //  private val configNextCloud = new HikariConfig()
