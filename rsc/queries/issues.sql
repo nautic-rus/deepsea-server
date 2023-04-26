50b19c103bf5d5b0123514dc15cb3e708e2cb91b
bogdan <Whatab0utus>
2023-04-26 10:06:23 +0300

    some changes

--- rsc/queries/issues.sql
+++
@@ -1,8 +0,0 @@
-select
-    *,
-    (select closing_status from issue_types where type_name = i.issue_type limit 1) as closing_status,
-    (select stage_date from issue_stages istages where istages.stage_name = i.period and istages.issue_type = i.issue_type and id_project in (select id from issue_projects ip where ip.name = i.project) limit 1) as stage_date
-from
-    issue i
-where
-    removed = 0
--- rsc/queries/userNotificationsByProject.sql
+++
@@ -1,4 +0,0 @@
-select email from users
-where doc_notification = 1 and id in
-    (select user_id from users_visibility_projects where project_id in
-        (select p.id from issue_projects p where p.name = '&project'))
