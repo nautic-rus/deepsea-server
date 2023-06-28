032200df2f54cbc81f3e14ba7af07a6c2decbe95
mamonov <SharrkSummoner>
2023-06-28 11:23:02 +0300

    edit docadded messages

---
+++ src/test/mamonov/TestNotifyDocUpload.scala
@@ -0,0 +1,6 @@
+import deepsea.issues.IssueManager.NotifyDocUpload
+import org.scalatest.funsuite.AnyFunSuite
+class TestNotifyDocUpload extends AnyFunSuite {
+//  val res = notifyDocUpload("10353", "common", "", "0")
+  val a = 0
+}
