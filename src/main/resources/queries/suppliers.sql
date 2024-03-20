36c7d4ed75afbca03db14252fffa6b980e5640ef
redwick <redeeming.fury@gmail.com>
2024-03-20 16:58:47 +0300

    some changes

--- src/main/resources/queries/suppliers.sql
+++ src/main/resources/queries/suppliers.sql
@@ -1,5 +1,5 @@
 SELECT
-    s.id as suppliers_id, s.user_id as user_id, s.equ_id as equip_id, s.name as equip_name, s.description equip_desc, s.comment as equip_comment, s.manufacturer as equip_manufacturer, ss.name as status, s.approvement
+    s.id as suppliers_id, s.user_id as user_id, s.equ_id as equip_id, s.name as equip_name, s.description equip_desc, s.comment as equip_comment, s.manufacturer as equip_manufacturer, ss.name as status, s.approvement, s.last_update
 FROM
     suppliers s
 LEFT JOIN suppliers_status ss ON s.status_id = ss.id
