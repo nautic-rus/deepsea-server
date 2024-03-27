174e6a7440123930b28ac7c44bbcb88d61bddec9
redwick <redeeming.fury@gmail.com>
2024-03-27 10:19:13 +0300

    some changes

--- src/main/resources/queries/related_tasks.sql
+++ src/main/resources/queries/related_tasks.sql
@@ -1,4 +1,4 @@
-SELECT sup_task_relations.id, i.id as issue_id, i.issue_type,i.issue_name, i.started_by, i.responsible, i.assigned_to, i.status
+SELECT sup_task_relations.id, i.id as issue_id, i.issue_type,i.issue_name, i.started_by, i.responsible, i.assigned_to, i.status, i.removed
 FROM sup_task_relations
          LEFT JOIN  issue i ON sup_task_relations.task_id = i.id
-WHERE sup_task_relations.suppliers_id = &supplier_id
+WHERE sup_task_relations.suppliers_id = &supplier_id  and i.removed = 0
