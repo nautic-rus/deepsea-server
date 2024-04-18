7f1d49ac6b8919f9e18c1972d3e9a044c1efead6
redwick <redeeming.fury@gmail.com>
2024-04-18 14:12:41 +0300

    some changes

--- src/main/scala/deepsea/storage/StorageHelper.scala
+++ src/main/scala/deepsea/storage/StorageHelper.scala
@@ -7,6 +7,7 @@ import io.circe.syntax.EncoderOps
 import slick.lifted.TableQuery
 import slick.jdbc.PostgresProfile.api._
 
+import java.util.Date
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Await
 import scala.concurrent.duration.{Duration, SECONDS}
@@ -17,7 +18,7 @@ trait StorageHelper {
     Await.result(DBManager.PostgresSQL.run(TableQuery[StorageUnitTable].insertOrUpdate(storageUnit)), Duration(5, SECONDS))
   }
   def updateStorageFile(storageFile: StorageFile): Unit = {
-    Await.result(DBManager.PostgresSQL.run(TableQuery[StorageFileTable].insertOrUpdate(storageFile)), Duration(5, SECONDS))
+    Await.result(DBManager.PostgresSQL.run(TableQuery[StorageFileTable].insertOrUpdate(storageFile.copy(date_created = new Date().getTime))), Duration(5, SECONDS))
   }
   def getStorageUnits: List[StorageUnit] = {
     Await.result(DBManager.PostgresSQL.run(TableQuery[StorageUnitTable].result.map(_.toList)), Duration(5, SECONDS)) match {
